e41930db61d7ea644f40892a1e336b1f0c540ac6
ckm <ckm@qq.com>
2022-08-03 18:07:43 +0800

    项目初始化

---
+++ gulimall-member/src/test/java/com/ckm/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckm.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
